8baac2d13b070cefc45a216703ecba08eb865dee
markubjo <markusbjon@gmail.com>
2020-02-24 15:52:27 +0100

    command line interface

--- client-server/cs_demo_client.c
+++ client-server/cs_demo_client.c
@@ -15,83 +15,95 @@ of its "Piece counter" node. The client accesses this data and prints it insinde
 //polling interval for the client
 #define SLEEP_TIME_MILLIS 2000000
 
-int main(void)
+int main(int argc, const char *argv[])
 {
 
-	/* First, we set up the connection */
+  if (argc < 2)
+  {
+    fprintf(stderr, "ERROR: No ip provided. Usage: ./client <end-point-ip>\n");
+    exit(EXIT_FAILURE);
+  }
 
-	// Create a new client
-	UA_Client *client = UA_Client_new();
-	// Set client config to default
-	UA_ClientConfig_setDefault(UA_Client_getConfig(client));
-	// Statuscode for client connection to spcified endpoint ("opc.tcp://<Raspberry pie ip-adress>:4840) 
-	UA_StatusCode retval = UA_Client_connect(client, "opc.tcp://127.0.01:4840"); // ip address 127.0.0.1 = This computer
+  char end_point[256];
+  strcat(end_point, "opc.tcp://");
+  strcat(end_point, argv[1]);
+  strcat(end_point, ":4840");
 
-	if (retval != UA_STATUSCODE_GOOD)
-	{
-		UA_Client_delete(client);
-		return (int)retval;
-	}
+  /* First, we set up the connection */
 
-	/* Next we will read the value attribute of the node */
+  // Create a new client
+  UA_Client *client = UA_Client_new();
+  // Set client config to default
+  UA_ClientConfig_setDefault(UA_Client_getConfig(client));
+  // Statuscode for client connection to spcified endpoint ("opc.tcp://<Raspberry pie ip-adress>:4840)
+  UA_StatusCode retval = UA_Client_connect(client, end_point); // ip address 127.0.0.1 = This computer" (opc.tcp://127.0.0.1:4840")
 
-	// Variants can hold scalar values and arrays of any type
-	UA_Int32 myInteger = 0;
-	UA_Variant value;
-	UA_Variant_init(&value);
-	UA_Variant_setScalar(&value, &myInteger, &UA_TYPES[UA_TYPES_INT32]);
+  if (retval != UA_STATUSCODE_GOOD)
+  {
+    UA_Client_delete(client);
+    return (int)retval;
+  }
 
-	// Browsing nodes in objects folder
-	UA_BrowseRequest bReq;
-	UA_BrowseRequest_init(&bReq);
+  /* Next we will read the value attribute of the node */
 
-	bReq.requestedMaxReferencesPerNode = 0;
-	bReq.nodesToBrowse = UA_BrowseDescription_new();
-	bReq.nodesToBrowseSize = 1;
-	// Browse objects folder
-	bReq.nodesToBrowse[0].nodeId = UA_NODEID_NUMERIC(0, UA_NS0ID_OBJECTSFOLDER);
-	// Return everything
-	bReq.nodesToBrowse[0].resultMask = UA_BROWSERESULTMASK_ALL;
-	UA_BrowseResponse bResp = UA_Client_Service_browse(client, bReq);
+  // Variants can hold scalar values and arrays of any type
+  UA_Int32 myInteger = 0;
+  UA_Variant value;
+  UA_Variant_init(&value);
+  UA_Variant_setScalar(&value, &myInteger, &UA_TYPES[UA_TYPES_INT32]);
 
-	/* Iterate through list looking for a specific node, the node representing 
+  // Browsing nodes in objects folder
+  UA_BrowseRequest bReq;
+  UA_BrowseRequest_init(&bReq);
+
+  bReq.requestedMaxReferencesPerNode = 0;
+  bReq.nodesToBrowse = UA_BrowseDescription_new();
+  bReq.nodesToBrowseSize = 1;
+  // Browse objects folder
+  bReq.nodesToBrowse[0].nodeId = UA_NODEID_NUMERIC(0, UA_NS0ID_OBJECTSFOLDER);
+  // Return everything
+  bReq.nodesToBrowse[0].resultMask = UA_BROWSERESULTMASK_ALL;
+  UA_BrowseResponse bResp = UA_Client_Service_browse(client, bReq);
+
+  /* Iterate through list looking for a specific node, the node representing 
 		 number of counted parts */
-	for (size_t i = 0; i < bResp.resultsSize; ++i)
-	{
-		for (size_t j = 0; j < bResp.results[i].referencesSize; ++j)
-		{
-			UA_ReferenceDescription *ref = &(bResp.results[i].references[j]);
-			if (ref->nodeId.nodeId.identifier.numeric == COUNTER_NODE_ID)
-			{
-				while (retval == UA_STATUSCODE_GOOD)
-				{
-					/* READ FROM NODE OPERATION */
-					// Reads the value of the node ID , puts it into "value"
-					retval = UA_Client_readValueAttribute(client, ref->nodeId.nodeId, &value);
-					if (retval == UA_STATUSCODE_GOOD &&	UA_Variant_hasScalarType(&value, &UA_TYPES[UA_TYPES_INT32]))
-					{
-						UA_Int32 raw_val = *(UA_Int32 *)value.data;
-						printf("\nRecieved from server: [Counted pieces] %i ", raw_val);
-					}
-					else
-					{
-						/* For debugging */
-						if (retval != UA_STATUSCODE_GOOD)	printf("\nretval != UA_STATUSCODE_GOOD\n");
-						if (!UA_Variant_hasScalarType(&value, &UA_TYPES[UA_TYPES_INT32]))
-						{
-							printf("\nUA_Variant_hasScalarType != OK\n");
-						}
-					}
-					usleep(SLEEP_TIME_MILLIS);
-				}
-			}
-		}
-	}
+  for (size_t i = 0; i < bResp.resultsSize; ++i)
+  {
+    for (size_t j = 0; j < bResp.results[i].referencesSize; ++j)
+    {
+      UA_ReferenceDescription *ref = &(bResp.results[i].references[j]);
+      if (ref->nodeId.nodeId.identifier.numeric == COUNTER_NODE_ID)
+      {
+        while (retval == UA_STATUSCODE_GOOD)
+        {
+          /* READ FROM NODE OPERATION */
+          // Reads the value of the node ID , puts it into "value"
+          retval = UA_Client_readValueAttribute(client, ref->nodeId.nodeId, &value);
+          if (retval == UA_STATUSCODE_GOOD && UA_Variant_hasScalarType(&value, &UA_TYPES[UA_TYPES_INT32]))
+          {
+            UA_Int32 raw_val = *(UA_Int32 *)value.data;
+            printf("\nRecieved from server: [Counted pieces] %i ", raw_val);
+          }
+          else
+          {
+            /* For debugging */
+            if (retval != UA_STATUSCODE_GOOD)
+              printf("\nretval != UA_STATUSCODE_GOOD\n");
+            if (!UA_Variant_hasScalarType(&value, &UA_TYPES[UA_TYPES_INT32]))
+            {
+              printf("\nUA_Variant_hasScalarType != OK\n");
+            }
+          }
+          usleep(SLEEP_TIME_MILLIS);
+        }
+      }
+    }
+  }
 
-	// clean up
-	UA_BrowseRequest_clear(&bReq);
-	UA_BrowseResponse_clear(&bResp);
-	UA_Variant_clear(&value);
-	UA_Client_delete(client); //Disconnects the client internally
-	return EXIT_SUCCESS;
+  // clean up
+  UA_BrowseRequest_clear(&bReq);
+  UA_BrowseResponse_clear(&bResp);
+  UA_Variant_clear(&value);
+  UA_Client_delete(client); //Disconnects the client internally
+  return EXIT_SUCCESS;
 }
--- client-server/cs_demo_server.c
+++ client-server/cs_demo_server.c
@@ -25,84 +25,84 @@ UA_Boolean running = true;
 static void stopHandler(int sig)
 {
   UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND, "Received ctrl-c");
-	running = false;
+  running = false;
 }
 
 static void addCounterSensorVariable(UA_Server *server)
 {
-	/* Here we are setting a specific ID for the node but the library
+  /* Here we are setting a specific ID for the node but the library
 	   can do it if we don't specify it */
-	UA_NodeId counterNodeId = UA_NODEID_NUMERIC(1, COUNTER_NODE_ID);
-	// We specify the name of the OPC UA node
-	UA_QualifiedName counterName = UA_QUALIFIEDNAME(1, "Piece Counter[pieces]");
+  UA_NodeId counterNodeId = UA_NODEID_NUMERIC(1, COUNTER_NODE_ID);
+  // We specify the name of the OPC UA node
+  UA_QualifiedName counterName = UA_QUALIFIEDNAME(1, "Piece Counter[pieces]");
 
-	// Giving the node some attributes
-	UA_VariableAttributes attr = UA_VariableAttributes_default;
-	attr.description = UA_LOCALIZEDTEXT("en_US", "Piece Counter (units:pieces)");
-	attr.displayName = UA_LOCALIZEDTEXT("en_US", "Piece Counter");
-	attr.dataType = UA_TYPES[UA_TYPES_INT32].typeId;
+  // Giving the node some attributes
+  UA_VariableAttributes attr = UA_VariableAttributes_default;
+  attr.description = UA_LOCALIZEDTEXT("en_US", "Piece Counter (units:pieces)");
+  attr.displayName = UA_LOCALIZEDTEXT("en_US", "Piece Counter");
+  attr.dataType = UA_TYPES[UA_TYPES_INT32].typeId;
 
-	// Set the initial value to 0
-	UA_Int32 counterValue = 0;
-	UA_Variant_setScalarCopy(&attr.value, &counterValue, &UA_TYPES[UA_TYPES_INT32]);
+  // Set the initial value to 0
+  UA_Int32 counterValue = 0;
+  UA_Variant_setScalarCopy(&attr.value, &counterValue, &UA_TYPES[UA_TYPES_INT32]);
 
-	// Include the variable to the server under the root object folder
-	UA_Server_addVariableNode(server, counterNodeId,
-														UA_NODEID_NUMERIC(0, UA_NS0ID_OBJECTSFOLDER),
-														UA_NODEID_NUMERIC(0, UA_NS0ID_ORGANIZES),
-														counterName, UA_NODEID_NULL, attr, NULL, NULL);
+  // Include the variable to the server under the root object folder
+  UA_Server_addVariableNode(server, counterNodeId,
+                            UA_NODEID_NUMERIC(0, UA_NS0ID_OBJECTSFOLDER),
+                            UA_NODEID_NUMERIC(0, UA_NS0ID_ORGANIZES),
+                            counterName, UA_NODEID_NULL, attr, NULL, NULL);
 }
 
 // Thread used to monitor the "sensor"
 void *mainSensor(void *ptr)
 {
-	UA_Server *server = ptr;
+  UA_Server *server = ptr;
 
-	while (running == 1)
-	{
-		// Update the counter
-		numberOfParts += 1;
-		printf("\nCounter updated: %i parts\n", numberOfParts);
+  while (running == 1)
+  {
+    // Update the counter
+    numberOfParts += 1;
+    printf("\nCounter updated: %i parts\n", numberOfParts);
 
-		// Update the OPC UA node
-		UA_Variant value;
-		UA_Int32 myInteger = (UA_Int32)numberOfParts;
-		UA_Variant_setScalarCopy(&value, &myInteger, &UA_TYPES[UA_TYPES_INT32]);
-		UA_Server_writeValue(server, UA_NODEID_NUMERIC(1, COUNTER_NODE_ID), value);
+    // Update the OPC UA node
+    UA_Variant value;
+    UA_Int32 myInteger = (UA_Int32)numberOfParts;
+    UA_Variant_setScalarCopy(&value, &myInteger, &UA_TYPES[UA_TYPES_INT32]);
+    UA_Server_writeValue(server, UA_NODEID_NUMERIC(1, COUNTER_NODE_ID), value);
 
-		usleep(SLEEP_TIME_MILLIS);
-	}
+    usleep(SLEEP_TIME_MILLIS);
+  }
 }
 
 int main(void)
 {
-	int ret;
-	pthread_t threadSensor;
+  int ret;
+  pthread_t threadSensor;
 
-	signal(SIGINT, stopHandler);
-	signal(SIGTERM, stopHandler);
+  signal(SIGINT, stopHandler);
+  signal(SIGTERM, stopHandler);
 
-	// Create a new server with default configuration
-	UA_Server *server = UA_Server_new();
-	UA_ServerConfig_setDefault(UA_Server_getConfig(server));
+  // Create a new server with default configuration
+  UA_Server *server = UA_Server_new();
+  UA_ServerConfig_setDefault(UA_Server_getConfig(server));
 
-	// Add the variable from the fake sensor
-	addCounterSensorVariable(server);
+  // Add the variable from the fake sensor
+  addCounterSensorVariable(server);
 
-	/* Launch the thread. The OPC UA server is passed as parameter as the
+  /* Launch the thread. The OPC UA server is passed as parameter as the
 	   value of the node needs to be updated. */
-	if (pthread_create(&threadSensor, NULL, mainSensor, server))
-	{
-		fprintf(stderr, "Error - pthread_create(): %d\n", ret);
-		exit(EXIT_FAILURE);
-	}
+  if (pthread_create(&threadSensor, NULL, mainSensor, server))
+  {
+    fprintf(stderr, "Error - pthread_create(): %d\n", ret);
+    exit(EXIT_FAILURE);
+  }
 
-	/* This line runs the server in a loop while the running variable is true. 
+  /* This line runs the server in a loop while the running variable is true. 
 	   It's important that initializations and other things done in our 
 	   code are before this function call. */
-	UA_StatusCode retval = UA_Server_run(server, &running);
+  UA_StatusCode retval = UA_Server_run(server, &running);
 
-	// When the server stops running we free the resources
-	UA_Server_delete(server);
-	return (int)retval;
+  // When the server stops running we free the resources
+  UA_Server_delete(server);
+  return (int)retval;
 }
